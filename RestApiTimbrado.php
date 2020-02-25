0d35c05906c9813120e9fce6d8989428bb59f13c
FacturoPorTi <50918852+facturoporti@users.noreply.github.com>
2020-02-25 01:08:39 -0600

    Se agrego https

--- RestApiTimbrado.php
+++ RestApiTimbrado.php
@@ -5,7 +5,7 @@ class CurlRequest
     {
         //url contra la que atacamos
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "http://wcfpruebas.facturoporti.com.mx/Timbrado/Servicios.svc/ApiTimbrarCFDI");
+        curl_setopt($ch, CURLOPT_URL, "https://wcfpruebas.facturoporti.com.mx/Timbrado/Servicios.svc/ApiTimbrarCFDI");
         //a true, obtendremos una respuesta de la url, en otro caso,
         //true si es correcto, false si no lo es
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
